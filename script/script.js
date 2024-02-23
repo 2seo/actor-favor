0c91a6306fdcfc73f476b177726d9a3d6af241a8
2seo <yuj07149@gmail.com>
2024-02-23 19:14:06 +0900

    Feat: 영화 검색 결과 반환 코드 작성

--- script/script.js
+++ script/script.js
@@ -4,32 +4,54 @@ const searchSection = document.querySelector(".search-section");
 const searchBtn = searchSection.querySelector("button");
 const searchValue = searchSection.querySelector("input");
 
-searchBtn.addEventListener('click' , () => {
+searchBtn.addEventListener('click', () => {
   const value = searchValue.value;
-  getCode(value);
+  getList(value);
 });
 
-const getCode = (movieName) => {
-  if(movieName===null || movieName==="") {
+var resultSection = document.querySelector(".result-section");
+var results = resultSection.querySelectorAll(".results");
+var buttons = resultSection.querySelectorAll("button");
+
+const getList = (movieName) => {
+  if (movieName === null || movieName === "") {
     alert("영화 제목을 입력하세요.")
   } else {
     fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${key}&movieNm=${movieName}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      getMovieInfo(data.movieListResult.movieList[0].movieCd);
-    })
-    .catch(error => console.log(error));
+      .then(res => res.json())
+      .then(data => {
+        returnList(movieName, data.movieListResult);
+      })
+      .catch(error => console.log(error));
   }
+}
+const returnList = (movieName, result) => {
+  const resultSection = document.querySelector(".result-section");
+  resultSection.innerHTML = `
+  <span>"${movieName}"의 검색결과(${result.totCnt}건)</span>
+  ${result.movieList.map((m) => {
+    return `
+        <div key=${m.movieCd} class="results">
+          <span>${m.movieNm}(${m.prdtYear})</span>
+          <span>${m.directors[0]?.peopleNm || ""}</span>
+          <button id=${m.movieCd} class="select-button">영화 선택</button>
+        </div>
+        `
+    })
+      }
+  `;
+  const buttons = [...document.querySelectorAll(".select-button")];
+  buttons.map((btn) => {
+    btn.addEventListener('click', ()=> {
+      getMovieInfo(btn.id);
+    })
+  })
+
 }
 
 const getMovieInfo = (movieCode) => {
   fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${key}&movieCd=${movieCode}`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 }
-
-  // 영화 이름 -> 영화 코드 가져와야함
-  // fetch 코드 함수화 하기
-  // key 따로 빼기
